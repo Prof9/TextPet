52faec34ce41388a74c85e009e46ad35705f3657
Prof. 9 <prof9@outlook.com>
2017-02-04 17:34:06 +0100

    Add --preserve-commands option to trim command.

--- TextPet/Commands/TrimCommand.cs
+++ TextPet/Commands/TrimCommand.cs
@@ -13,19 +13,22 @@ namespace TextPet.Commands {
 		private const string maxArg = "max";
 		private const string amountArg = "amount";
 		private const string preserveTextArg = "preserve-text";
+		private const string preserveCommandsArg = "preserve-commands";
 
 		public TrimCommand(CommandLineInterface cli, TextPetCore core)
 			: base(cli, core, new string[0], new OptionalArgument[] {
 				new OptionalArgument(maxArg, 'm', new string[] {
 					amountArg,
 				}),
-				new OptionalArgument(preserveTextArg, 'p'),
+				new OptionalArgument(preserveTextArg, 't'),
+				new OptionalArgument(preserveCommandsArg, 'c'),
 			}) { }
 
 		protected override void RunImplementation() {
 			string maxStr = GetOptionalValues(maxArg)?[0];
 			int max = maxStr != null ? NumberParser.ParseInt32(maxStr) : int.MaxValue;
 			bool preserveText = GetOptionalValues(preserveTextArg) != null;
+			bool preserveCommands = GetOptionalValues(preserveCommandsArg) != null;
 
 			// Cycle through every script in every text archive.
 			foreach (TextArchive ta in this.Core.TextArchives) {
@@ -39,13 +42,20 @@ namespace TextPet.Commands {
 						}
 					}
 
-					// Remove commands from the end of the script until the script-ending command or maximum is reached.
+					// Remove elements from the end of the script until the script-ending element or maximum is reached.
 					int trimmed = 0;
 					while (script.Count > endPos && (maxStr == null || trimmed < max)) {
 						// If the preverse text option is enabled, cancel if this would trim non-whitespace text.
 						if (preserveText) {
 							TextElement textElem = script[script.Count - 1] as TextElement;
 							if (textElem != null && !textElem.Text.All(c => Char.IsWhiteSpace(c))) {
+								break;
+							}
+						}
+						// If the preserve commands option is enabled, cancel if this would trim commands.
+						if (preserveCommands) {
+							Command cmdElem = script[script.Count - 1] as Command;
+							if (cmdElem != null) {
 								break;
 							}
 						}
